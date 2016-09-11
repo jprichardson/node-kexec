0f3ec1e0328241edf63a87f944e497bcde16d08f
JP Richardson <jprichardson@gmail.com>
2016-09-11 08:35:51 -0500

    index: style change

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var kexec = null;
-
-/*try {
-  kexec = require("./build/default/kexec.node");
-} catch(e) {*/
-  kexec = require("./build/Release/kexec.node");
-//}
-
-module.exports = kexec.kexec; //function of kexec module is named kexec
+module.exports = require('./build/Release/kexec.node').kexec
